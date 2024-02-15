685a9c2afcd28fa19ea896a673cae52afe355d51
zebratul <102689746+zebratul@users.noreply.github.com>
2024-02-15 11:53:01 +0300

    Sequelize integration

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const { startServer } = require("./server");
 const logger = require("./services/logger.service")(module);
+require('dotenv').config();
 // todo: разкомментировать для работы с БД
-// const { sampleDB } = require("./services/database.service");
+const { Database } = require("./services/database.service"); 
+const connectionString = process.env.DATABASE_URL;
+const database = new Database(connectionString);
 
 (async () => {
   try {
     // todo: разкомментировать для работы с БД
-    // await sampleDB.connect();
+    await database.connect();
     startServer();
   } catch (error) {
     logger.error(error.message);
     // todo: разкомментировать для работы с БД
-    // await sampleDB.disconnect();
+    await database.disconnect();
     logger.shutdown(() => process.exit(1));
   }
 })();
@@ -19,7 +22,7 @@ const logger = require("./services/logger.service")(module);
 ["SIGINT", "SIGTERM", "SIGQUIT"].forEach((signal) =>
   process.on(signal, async () => {
     // todo: разкомментировать для работы с БД
-    // await sampleDB.disconnect();
+    await database.disconnect();
     logger.info(`Caught signal ${signal}`);
     logger.shutdown(() => process.exit(0));
   })
@@ -27,7 +30,7 @@ const logger = require("./services/logger.service")(module);
 
 process.on("uncaughtException", async (error) => {
   // todo: разкомментировать для работы с БД
-  // await sampleDB.disconnect();
+  await database.disconnect();
   logger.error(`Uncaught exception! ${error.message}`);
   logger.shutdown(() => process.exit(1));
 });
--- services/database.service.js
+++ services/database.service.js
@@ -1,78 +1,64 @@
-const dbsConfig = require("../config").dbs;
-const logger = require("./logger.service")(module);
-require('dotenv').config();
 const { Sequelize } = require('sequelize');
+const logger = require("./logger.service")(module);
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  dialectOptions: {
-      ssl: {
+class Database {
+  /**
+  * Базовый класс сервиса работы с базами данных через Sequelize
+  */
+  #sequelize;
+
+  constructor(connectionString) {
+    this.#sequelize = new Sequelize(connectionString, {
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
           require: true,
           rejectUnauthorized: false,
+        },
       },
-  },
-  define: {
-      timestamps: true,
-      underscored: true,
-  },
-  logging: (msg) => logger.debug(msg),
-});
-
-/**
- * Базовый класс сервиса работы с базой данных
- */
-class Database {
-  #uri;
-
-  #id;
-
-  #database;
-
-  #connection;
-
-  constructor(config) {
-    this.#uri = config.uri;
-    this.#id = config.id;
-    this.#database = config.database;
+      define: {
+        timestamps: true,
+        underscored: true,
+      },
+      logging: (msg) => logger.debug(msg),
+    });
   }
 
   /**
-   * Открывает соединение с БД.
+   * Проверка соединения с базой. Sequelize пуллит соединения автоматически.
    * @return {Promise<void>}
    */
   async connect() {
     try {
-      // todo: метод установки соединения с БД
-      logger.info(`Connected to ${this.#id}`);
+      await this.#sequelize.authenticate();
+      logger.info(`Connection to the database has been established successfully.`);
     } catch (error) {
-      logger.error(`Unable to connect to ${this.#id}:`, error.message);
+      logger.error(`Unable to connect to the database:`, error.message);
+      throw error;
     }
   }
 
   /**
-   * Закрывает соединение с БД.
+   * Закрывает соединение.
    * @return {Promise<void>}
    */
   async disconnect() {
-    if (this.#connection) {
-      try {
-        // todo: метод закрытия соединения с БД
-        logger.info(`Disconnected from ${this.#id}`);
-      } catch (error) {
-        logger.error(`Unable to disconnect from ${this.#id}:`, error.message);
-      }
+    try {
+      await this.#sequelize.close();
+      logger.info(`Connection to the database has been closed successfully.`);
+    } catch (error) {
+      logger.error(`Error closing the database connection:`, error.message);
+      throw error;
     }
   }
 
   /**
-   * Возвращает объект соединения с БД,
-   * @return {Object}
+   * Возвращает объект соединения с БД.
+   * @return {Sequelize}
    */
-  get connection() {
-    return this.#connection;
+  get sequelize() {
+    return this.#sequelize;
   }
 }
 
-const sampleDB = new Database(dbsConfig.sample_db);
-
-module.exports = { sampleDB, sequelize };
+module.exports = { Database };
